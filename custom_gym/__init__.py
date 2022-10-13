1325c462be8b2472fe954705d5d45c1e0ff8a04f
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2022-10-13 15:22:31 -0400

    updated init

--- custom_gym/__init__.py
+++ custom_gym/__init__.py
@@ -13,16 +13,55 @@ register(
     id='custom_gym/GridWorld-v0',
     entry_point='custom_gym.envs:GridWorldEnv',
     max_episode_steps=100,
+    reward_threshold = None,
+    nondeterministic = False,
+    order_enforce = True,
+    autoreset = False,
+    kwargs = 
+    {
+        "agent": [20,20],
+        "dimensions": [40,40],
+        "goal": [10,10],
+        "p": 0.1,
+        "prefix": "/home/jared/ambiguity_ws/data/gridworld/",
+        "reward_bounds": [0,1]
+    }
 )
 
 register(
     id='custom_gym/GridTunnel-v0',
     entry_point='custom_gym.envs:GridTunnelEnv',
     max_episode_steps=100,
+    reward_threshold = None,
+    nondeterministic = False,
+    order_enforce = True,
+    autoreset = False,
+    kwargs = 
+    {
+        "agent": [20,20],
+        "dimensions": [35,10],
+        "goal": [10,10],
+        "p": 0.1,
+        "prefix": "/home/jared/ambiguity_ws/data/gridworld/",
+        "reward_bounds": [0,1]
+    }
 )
 
 register(
     id='custom_gym/Sailing-v0',
     entry_point='custom_gym.envs:SailingEnv',
     max_episode_steps=100,
+    reward_threshold = None,
+    nondeterministic = False,
+    order_enforce = True,
+    autoreset = False,
+    kwargs = 
+    {
+        "agent": [20,20],
+        "dimensions": [40,40],
+        "goal": [10,10],
+        "p": 0.1,
+        "prefix": "/home/jared/ambiguity_ws/data/gridworld/",
+        "reward_bounds": [0,1]
+    }
 )
