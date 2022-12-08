2fa5e30943e186811f9c4b55764a746e07b1c0b1
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2022-12-08 13:58:17 -0500

    rm potential envs

--- irl_gym/envs/wrappers/multistep.py
+++
@@ -1,5 +0,0 @@
-#pass in env params, and will wrap and perform multiple timesteps.
-
-#init env.
-# then in step function perform multiple timesteps. 
-    #idea of doing it this way is that we can still leverage truncation in gym.
