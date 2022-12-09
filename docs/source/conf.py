aa63312bc60aafbf1c2e5fb5562c0809579fd174
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2022-12-09 11:53:05 -0500

    tried stack overflow fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 
 project = 'irl-gym'
 copyright = '2022, Jared J. Beard, R. Michael Butts, Yu Gu'
