1bca0c51b1d7f4f4a4a0d773c17459fe9f2f7e53
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2022-12-09 10:59:40 -0500

    update to conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+  html_theme = 'sphinx_rtd_theme'
+  html_static_path = ['_static']
