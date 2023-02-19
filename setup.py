d258cfcc20b0effb6a90fb6866c3bf317e896fa3
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-18 19:40:37 -0500

    rm setup reqts

--- setup.py
+++ setup.py
@@ -7,7 +7,5 @@ setup(
     ),
     name="irl-gym",
     version="2.0.0",
-    install_requires=["numpy", 
-                      "gymnasium", 
-                      "pygame>=2.1.0"],
+    install_requires=[],
 )
