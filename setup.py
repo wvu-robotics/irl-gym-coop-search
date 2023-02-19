72b650a47736fb009f987abf5880bf7bd1c31b29
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-18 19:10:46 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
+    packages=find_packages(
+        exclude=["docs", "plot"]
+    ),
     name="irl-gym",
     version="2.0.0",
     install_requires=["numpy", 
