65cbbd4b61d0b5d720865f1c18dbcdbcc186e25c
matt <mac00072@mix.wvu.edu>
2023-04-27 10:25:52 -0400

    added obstacles

---
+++ gym_coop_search/test/obstacles.py
@@ -0,0 +1,20 @@
+
+# Generates obstacles within the environment
+
+# Can add different environments here
+
+import numpy as np
+
+def obstacles1(size_x, size_y, start, goal): # Env notes: x = 42, y = 25, start = [7, 10], goal = [8, 3]
+    obs = np.zeros((size_x, size_y))
+    obs[3, 16:22] = 1.0
+    return obs
+
+def obstacles2(size_x, size_y, start, goal): # Env notes: x = 42, y = 25, start = [7, 10], goal = [8, 3]
+    obs = np.zeros((size_x, size_y))
+    obs[4:9, 9] = 1.0
+    obs[4, 9:18] = 1.0
+    obs[9, 9:20] = 1.0
+    return obs
+
+
